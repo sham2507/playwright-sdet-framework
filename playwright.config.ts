86ba9159b11e8f96d1a33064e5797bcbf5f9fbf2
elixir <shamchellam07@gmail.com>
2025-12-23 10:41:02 +0530

    fix: run tests in headless mode

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
 
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'retain-on-failure',
